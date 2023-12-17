4d2bb739da29293d7beab59c490788025c1bbafd
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2023-12-17 11:32:57 -0500

    Add files via upload

---
+++ CCC/2015/2 -Jerseys.py
@@ -0,0 +1,23 @@
+# https://www.cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2015/stage%201/seniorEn.pdf
+# INCOMPLETE
+
+def main():
+    J = int(input("# of jerseys: "))
+    A = int(input("# of athletes: "))
+
+    print("Getting size of jerseys")
+
+    jerseys = []
+    requested = []
+
+    for j in range(J):
+        jerseys.append(input(f"Jersey #{j}'s size: "))
+    
+    for a in range(A):
+        # simpler version
+        input = input(f"Input request {a}: ")
+        requested.append(tuple(input.split()))
+    
+
+if __name__ == "__main__":
+    main()
