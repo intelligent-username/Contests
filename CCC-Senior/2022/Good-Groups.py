15ba4a523e72ff195834a7deb91572ba016f3975
Ron <varakt.23@gmail.com>
2024-01-20 12:54:23 -0500

    New Completions

--- CCC-Senior/2022/Good-Groups.py
+++ CCC-Senior/2022/Good-Groups.py
@@ -1,4 +1,4 @@
-# GOOD GROUPS; INCOMPLETE
+# GOOD GROUPS; COMPLETE
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2022/ccc/seniorEF.pdf #2
 # Again not following exact specific input accepting format in the terminal, can simply remove prompts inside of the quotations marks to do so. 
 
@@ -6,28 +6,29 @@ must_be = []
 must_not_be = []
 groups = []
 
-X = int(input("# of Student Combinations that MUST be in the same group: ")) # Numbers of pairs that must be in the same group
+X = int(input("# of Student Combinations that MUST be: ")) 
 
 for i in range(X):
-    must_be.append(input(f"Must be same group input #{i+1}: "))
+    must_be.append(set(input(f"Must be same group input #{i + 1}: ").split()))
 
-Y = int(input("# Of student Combinations that MUST NOT be in the same group: ")) # Number of pairs that must not be in the same group
+Y = int(input("# Of student Combinations that MUST NOT be: ")) 
+
 for j in range(Y):
-    must_not_be.append(input(f"Must not be together input #{j+1}: "))
+    must_not_be.append(set(input(f"Must not be together input #{j + 1}: ").split()))
 
-G = int(input("Number of Groups MADE: ")) # Number of groups
+G = int(input("Number of Groups MADE: ")) 
+
 for k in range(G):
-    groups.append(input(f"Group #{k+1}: "))
+    groups.append(set(input(f"Group #{k + 1}: ").split()))
 
 violations = 0
 
-# NOTE: RN it's checking the EXACT string rather than the two/three letter in the string  (ie if F E is required F D E won't detect as requirement met. Finish later)
 for required in must_be:
-    if required not in groups:
+    if not any(required.issubset(group) for group in groups):
         violations += 1
-print(violations)
-for illegals in must_not_be:
-    if illegals in groups:
-        violations += 1    
 
-print(violations)
+for illegals in must_not_be:
+    if any(illegals.issubset(group) for group in groups):
+        violations += 1
+
+print("Violated", violations, "Times")
