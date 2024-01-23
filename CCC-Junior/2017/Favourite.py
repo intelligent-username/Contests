7ca9c60d79dd3bd90ad61619dbfcccdd2b27b133
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2024-01-23 23:04:26 +0000

    Aoiuifdi

--- CCC-Junior/2017/Favourite.py
+++ CCC-Junior/2017/Favourite.py
@@ -7,9 +7,23 @@
 # Figure out how to encode time
 # Figure out what the max increments of arithmetic series are (6? 4?)
 # Then it's just simple math
-Time = [12, 00]
-Passed = int(input()) # The number of minutes passed
-Hours = Passed // 60
-Minutes = Passed - Hours * 60
+def main():
+    Noon = [12, 00]
+    Passed = int(input()) # The number of minutes passed
+
+    Hours = Passed // 60
+    Minutes = Passed - Hours * 60
+    
+    Endtime = [Noon[0] + Hours, Noon[1] + Minutes]
+    
+    palindrome_counter = 0
 
 
+
+
+def arithmetic_checker(time):
+    if int(time[3]) - int(time[2]) == int(time[1]) - time[0] and time[2] - time[1] == time[3] - time[2]:
+        return True
+
+if __name__ == "__main__":
+    main()
