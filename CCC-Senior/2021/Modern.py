bb31f4b5e1aed289af00aec731cc9242039c3fb3
Ron <varakt.23@gmail.com>
2024-01-23 19:53:17 -0500

    Aaaaaaaaaaaaa

--- CCC-Senior/2021/Modern.py
+++ CCC-Senior/2021/Modern.py
@@ -1,10 +1,44 @@
 # Modern Art; IN PROGRESS
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2021/ccc/seniorEF.pdf #2
 
+# Number of rows
 M = int(input())
 
+# Number of columns
 N = int(input())
 
+# Artist's choices
 K = int(input())
 
+Canvas = [["B"] * N] * M
+print("Canvas is", Canvas)
 
+for x in range(K):
+    request = input().split()
+    direction = request[0]
+    index = int(request[1])
+
+    if direction == "R":
+        # Then do row
+        for x in range(N):
+            if Canvas[index][x] == "B":
+                Canvas[index][x] = "G"
+            else:
+                Canvas[index][x] = "B"
+
+            print("Canvas is ", Canvas)
+
+    else:
+        # Else it's "C", do column
+        for j in Canvas:
+            # print("J is ", j)
+            if j[index] == "B":
+                j[index] = "G"
+            else:
+                j[index] = "B"
+            print("Canvas is", Canvas)
+            
+
+print(Canvas)
+answer = Canvas.count("G")
+print(answer)
