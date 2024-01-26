4ed509e5141a70c4d4c90e496bb769a2049f6e3c
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2024-01-26 18:06:55 +0000

    Afoiffoerufigreuf

---
+++ CCC-Junior/2016/Magic.py
@@ -0,0 +1,55 @@
+# Magic Squares; IN PROGRESS
+# https://www.cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2016/stage%201/juniorEn.pdf #2
+def main():
+    square = []
+    squares.append(list(map(int(),input().split())))
+    squares.append(list(map(int(),input().split())))
+    squares.append(list(map(int(),input().split())))
+    squares.append(list(map(int(),input().split())))
+
+    str = checker(square)
+    print(str)
+
+def checker(square):
+    sum = -0.01
+
+    # Check rows first
+    for x in square:
+        row = sum(x)
+        if row == 0:
+            sum = row
+        elif row != sum:
+    temp        return "not magic"
+
+    tempsum = 0
+
+    # Check columns
+    for x in range(4):
+        tempsum += squares[x][0]
+
+    if tempsum != sum:
+        return "not magic"
+
+    # Check diagonal 1
+
+    tempsum2 = 0
+
+    for x in range(4):
+        tempsum2 += square[x][x]
+    
+    if sum != tempsum2:
+        return "not magic"
+    
+    tempsum3 = 0
+
+    for x in range(3, -1, -1):
+        tempsum3 += square[x][x]
+
+    if tempsum3 != sum:
+        return "not magic" 
+
+    return "magic"
+
+if __name__ == "__main__":
+    main()
+
