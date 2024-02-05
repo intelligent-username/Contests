af1b92e062dc891378c39e9d0b3859438dcc33c4
Ron <varakt.23@gmail.com>
2024-02-05 23:02:34 +0000

    Feb 5, 2024, 6:02 PM

--- CCC-Junior/2023/Word.py
+++ CCC-Junior/2023/Word.py
@@ -1,22 +1,41 @@
 # WORD HUNT: IN PROGRESS
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2023/ccc/juniorEF.pdf #5
 
-# String of Uppercase letters (TARGET WORD)
-W = input()
+def main():
+    # String of Uppercase letters (TARGET WORD)
+    W = input()
 
-# Number of rows in the grid
-R = int(input())
+    # Number of rows in the grid
+    R = int(input())
 
-# Number of columns in the grid
-C = int(input())
-grid = []
+    # Number of columns in the grid
+    C = int(input())
+    grid = []
 
-# Number of times the word appears
-H = 0
+    # Number of times the word appears
+    H = 0
 
-for _ in range(R):
-    grid.append(input().split())
+    for _ in range(R):
+        grid.append(input().split())
+
+    for r in range(R):
+        check_horizontals(W, grid, R, C)
 
 
+    print(H)
 
-print(H)
+def check_horizontals(word, grid, rows, columns, move = False):
+    for row in range(rows):
+        for column in range(columns):
+            if grid[row][column] == word[column]:
+                
+                
+
+def check_verticals(word, grid, rows, columns):
+    ...
+
+def check_diagonals(word, grid, rows, columns):
+    ...
+
+if __name__ == "__main__":
+    main()
