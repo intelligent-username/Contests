c7fb75c6f8c03f8651c2879112c7d066732fa839
Ron <varakt.23@gmail.com>
2024-02-05 17:46:01 -0500

    Simple Synchronization

--- CCC-Junior/2023/Trianglane.py
+++ CCC-Junior/2023/Trianglane.py
@@ -18,3 +18,42 @@ row2.append(False)
 # Initiate variables
 total_meters = 0
 redundancy = 0
+
+# Counting of metres of tape
+for i in range(columns):
+    
+    if row1[i]:
+        total_meters += 3
+        # Base case, each triangle needs 3
+
+        if row1[i +1]:
+            redundancy += 1
+            # If the item to the right of the triangle is painted, one less metre is needed
+
+        if (i > 0) and row1[i-1]:
+            redundancy += 1
+            # Given that there is an item to the left of the triangle, check for redundancy
+            
+        if not (i % 2) and row2[i]:
+            redundancy += 1
+            # Only the evenly-indexed triangles will make contact with the row below to cause a redundancy
+
+    if row2[i]:
+        total_meters += 3
+        # Base Case
+
+        if row2[i+1]:
+            redundancy +=1
+        # Check the right
+
+        if (i > 0) and row2[i - 1]:
+            redundancy += 1
+            # Check left if applicable
+        
+        if not (i % 2) and row1[i]:
+            redundancy += 1
+            # Only the evenly-indexed triangles will make contact with the row above to cause a redundancy            
+        
+
+total_meters = total_meters - redundancy
+print(total_meters)
