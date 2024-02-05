d6c9f73ac2a74dc51989a50db89796318e1c85ac
Ron <varakt.23@gmail.com>
2024-02-05 22:42:40 +0000

    Feb 5, 2024, 5:42 PM

---
+++ CCC-Junior/2023/Word.py
@@ -0,0 +1,18 @@
+# WORD HUNT IN PROGRESS
+# https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2023/ccc/juniorEF.pdf #5
+
+# String of Uppercase letters (TARGET WORD)
+W = input()
+
+# Number of rows in the grid
+R = int(input())
+
+# Number of columns in the grid
+C = int(input())
+grid = []
+
+for _ in range(R):
+    grid.append(list(map(int, input().split())))
+
+for x in grid:
+    print("Grid:", x)
