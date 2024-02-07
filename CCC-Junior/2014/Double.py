175c05b46c8e69974ca4b1ac55950521e32a714d
Ron <varakt.23@gmail.com>
2024-02-07 22:42:53 +0000

    Feb 7, 2024, 5:42 PM

--- CCC-Junior/2014/Double.py
+++ CCC-Junior/2014/Double.py
@@ -1,3 +1,6 @@
+# DOUBLE DICE: IN PROGRESS
+# https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2014/stage%201/juniorEn.pdf #3
+
 p1 = 100
 p2 = 100
 
@@ -9,10 +12,10 @@ for x in range(N):
 
 for x in range(0, len(rolls), 2):
     print(x)
-    if x[0] > x[1]:
-        p2 -= x[0]
-    elif x[1] > x[0]:
-        p1 -= x[1]
+    if int(rolls[2*x]) > int(rolls[(2*x)+1]):
+        p2 -= int(rolls[(2*x)])
+    elif int(rolls[2*x]) < int(rolls[(2*x)+1]):
+        p1 -= int(rolls[(2*x)+1])
 
 print(p1)
 print(p2)
