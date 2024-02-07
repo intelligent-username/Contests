fd26cf218a32ab6a56ee554ac4bba5f2b1dab95c
Ron <varakt.23@gmail.com>
2024-02-07 22:36:33 +0000

    Feb 7, 2024, 5:36 PM

--- CCC-Senior/2014/party.py
+++ CCC-Senior/2014/party.py
@@ -1,25 +1,25 @@
 # Party Inivitation: In Progress
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2014/stage%201/seniorEn.pdf #1
 
-
-k = int(input())
-m = int(input())
+k = int(input()) # Number of friends
+m = int(input()) # 'm' rounds of removals
 
 friends = list(range(1, k + 1))
-print(friends)
+print("The list is rn:", friends)
 
 for j in range(m):
-    multiples = int(input())
+    multiples = int(input()) # Remove multiples of this number
     temp = 1
-    while True:
-        try:
-            friends[(multiples * temp) - 1] = False
-            temp += 1
-        except:
-            break
+    while temp * multiples < len(friends):
+        friends[(multiples * temp) - 1] = False
+        temp += 1
+
+    print("Iteration", j, "of the list:", friends)
+
     for x in friends:
         if not x: #if x i false
             friends.pop(x)
+    print("List after removal:", friends)
 
 for r in friends:
     print(r)
