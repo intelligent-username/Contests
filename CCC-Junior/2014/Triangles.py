852fcf06d4480042635038916952859b51641146
Ron <varakt.23@gmail.com>
2024-02-07 23:19:13 +0000

    Feb 7, 2024, 6:19 PM

---
+++ CCC-Junior/2014/Triangles.py
@@ -0,0 +1,9 @@
+# TRIANGLE TIMES: IN PROGRESS
+# https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2014/stage%201/juniorEn.pdf #1
+
+angle1 = int(input())
+angle2 = int(input())
+angle3 = int(input())
+
+if angle1 + angle2 + angle3 != 180:
+    print("Error")
