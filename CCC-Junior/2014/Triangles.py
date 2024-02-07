d674d58cb0aaf853533734b1d84e831597dde4a6
Ron <varakt.23@gmail.com>
2024-02-07 23:19:52 +0000

    Feb 7, 2024, 6:19 PM

--- CCC-Junior/2014/Triangles.py
+++ CCC-Junior/2014/Triangles.py
@@ -7,3 +7,9 @@ angle3 = int(input())
 
 if angle1 + angle2 + angle3 != 180:
     print("Error")
+elif angle1 == angle2 == angle3:
+    print("Equilateral")
+elif angle1 == angle2 or angle1 == angle3 or angle2 == angle3:
+    print("Isosceles")
+else:
+    print("Scalene")
