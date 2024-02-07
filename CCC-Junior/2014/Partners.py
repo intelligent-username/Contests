ca920054bffb8514e07ffd69a8fad448a5d9ef40
Ron <varakt.23@gmail.com>
2024-02-07 22:58:35 +0000

    Feb 7, 2024, 5:58 PM

--- CCC-Junior/2014/Partners.py
+++ CCC-Junior/2014/Partners.py
@@ -1,18 +1,30 @@
 # Assigning Partners; IN PROGRESS
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2014/stage%201/juniorEn.pdf #5
+def main1():
+    print(main2())
 
-N = int(input()) # Number of students in the class
+def main2():
+    N = int(input()) # Number of students in the class
 
-stu_1 = input().split() # Names of N students separated by single spaces; no two students have same name don't even trip
+    stu_1 = input().split() # Names of N students separated by single spaces; no two students have same name don't even trip
 
-stu_2 = input().split() # Partner of each student. 
+    stu_2 = input().split() # Partner of each student. 
 
-# The student in position i in stu_1 is the parter of the student in position i in stu_2, and vice versa
-# Also they can't partner with theyselves
+    # The student in position i in stu_1 is the parter of the student in position i in stu_2, and vice versa
+    # Also they can't partner with theyselves
 
-pairs = set()
+    pairs = set()
 
-for x in range(len(stu_1)):
-    pairs.append(tuple(stu_1[x], stu_2[x]))
+    for x in range(N):
+        pairs.append(tuple(stu_1[x], stu_2[x]))
 
+    pairs = list(pairs)
 
+    for name in stu_1:
+        counter = pairs.count(name)
+        if counter > 1:
+            return "bad"
+
+    return "good"
+
+main1()
