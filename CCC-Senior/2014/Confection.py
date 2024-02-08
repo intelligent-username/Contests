3a0dbf1a6247e05743e9883a0fcb19eaa717a1c2
Ron <varakt.23@gmail.com>
2024-02-08 00:14:35 +0000

    Feb 7, 2024, 7:14 PM

--- CCC-Senior/2014/Confection.py
+++ CCC-Senior/2014/Confection.py
@@ -2,6 +2,7 @@
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2014/stage%201/seniorEn.pdf #3
 def main():
     T = int(input()) # # of tests
+    answers = []
 
     for _ in range(T):
         N = int(input()) # Number of cars
@@ -12,32 +13,37 @@ def main():
             # The cars will always use the numbers from 1 to N in order.
             # Need to figure out if I can put them in from 1 to 4
         
-        print(sliderchecker(cars, N))
+        answers.append(sliderchecker(cars, N))
+    
+    for x in answers:
+        print(x)
 
 # This function will check if the cars can be stacked in 1,2,3....N order
 def sliderchecker(cars, N):
     highest_rn = 0
     branch = []
-    for q in range(N-1, 0, -1):
+
+    for q in range(N-1, -1, -1):  # Fix the range to include the first element
         if cars[q] == highest_rn + 1:
             highest_rn += 1
-        elif branch[0] == highest_rn + 1:
+        elif branch and branch[-1] == highest_rn + 1:
             highest_rn += 1
             branch.pop()
         else:
-            branch.append(cars[q])
-            cars.remove(q)
-    if not highest_rn and not branch:
+            branch.append(cars.pop(q))  # Use pop to remove by index
+
+    if highest_rn == N and not branch:
         return "Y"
-    print("Branch is", branch)
+    
     while branch:
-        if branch[0] == highest_rn + 1:
+        if branch[-1] == highest_rn + 1:
             highest_rn += 1
             branch.pop()
         else:
             return "N"
-            
+
     return "Y"
+
 
 if __name__ == "__main__":
     main()
