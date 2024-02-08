7975b8a1c34acdc74914e8c8df13c456434505e6
Ron <varakt.23@gmail.com>
2024-02-08 01:17:57 +0000

    Feb 7, 2024, 8:17 PM

---
+++ CCC-Senior/2006/Maternity.py
@@ -0,0 +1,14 @@
+# Maternity; IN PROGRESS
+# https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2006/stage1/seniorEn.pdf #1
+
+# Doing an easier question for a change
+
+p1 = input()
+p2 = input()
+
+# Number of offspring to check
+X = int(input())
+offspring = []
+
+for _ in range(X):
+    offspring.append(input())
