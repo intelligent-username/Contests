4689863a3d74ba5066f874a00da39b3614dfbe12
Ron <varakt.23@gmail.com>
2024-02-08 00:24:05 +0000

    Feb 7, 2024, 7:24 PM

--- CCC-Senior/2014/Confection.py
+++ CCC-Senior/2014/Confection.py
@@ -26,7 +26,8 @@ def sliderchecker(cars, N):
     for q in range(N-1, -1, -1):  # Fix the range to include the first element
         if cars[q] == highest_rn + 1:
             highest_rn += 1
-        elif branch and branch[-1] == highest_rn + 1:
+            del cars[q]
+        elif branch[-1] == highest_rn + 1:
             highest_rn += 1
             branch.pop()
         else:
