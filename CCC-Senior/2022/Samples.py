83d11f049d64b06becf44dfd56f6c3b23415109c
Ron <varakt.23@gmail.com>
2024-02-08 01:02:16 +0000

    Feb 7, 2024, 8:02 PM

--- CCC-Senior/2022/Samples.py
+++ CCC-Senior/2022/Samples.py
@@ -1,4 +1,14 @@
+# Good Samples; IN PROGRESS
+# https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2022/ccc/seniorEF.pdf #3
+
 N, M, K = input().split()
 
+# Makes a piece with exactly N notes
+# Positive int is the pitch of the note
+# A sample is good if no two notes in the sample have the same pitch
+
+# N: number of notes
+# M: highest accepted pitch
+# A piece with exactly K good samples is required
 
 
