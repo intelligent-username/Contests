cf19f167d1250cd0d686fe6fe83b7f4b0d7ab3a5
Ron <varakt.23@gmail.com>
2024-02-08 01:24:18 +0000

    Feb 7, 2024, 8:24 PM

--- CCC-Senior/2006/Maternity.py
+++ CCC-Senior/2006/Maternity.py
@@ -12,3 +12,29 @@ offspring = []
 
 for _ in range(X):
     offspring.append(input())
+
+possibilities = []
+
+for each in offspring:
+    temp = True # Be optimistic and assume it is their child
+    for char in each:
+        if char.isupper():
+            if char in p1 or char in p2:
+                continue
+            else:
+                temp = False
+        else:
+            if char in p1 and char in p2:
+                continue
+            else: 
+                temp = False
+
+        if not temp:
+            possibilities.append(False)
+            break
+
+for x in possibilities:
+    if x:
+        print("Possible baby")
+    else:
+        print("Not their baby!")
