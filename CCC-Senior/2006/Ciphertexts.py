c846f3d62f150836bb24672e5f121e97fcfbeed9
Ron <varakt.23@gmail.com>
2024-02-08 19:10:27 +0000

    Feb 8, 2024, 2:10 PM

--- CCC-Senior/2006/Ciphertexts.py
+++ CCC-Senior/2006/Ciphertexts.py
@@ -1,4 +1,14 @@
 # Attack of the Ciphertexts; IN PROGRESS
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2006/stage1/seniorEn.pdf #1 
 
-pt1 = 
+pt1 = input()
+ct1 = input()
+pt2 = ""
+ct2 = input()
+
+# Use plaintext1 and cyphertext one to map letters to each other. 
+# Then use the mapping of the letters to form plaintext2
+# And then print plaintext2
+
+keys = {}
+
