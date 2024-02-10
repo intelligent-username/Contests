edf6831f214708d3327f589ca6485aa2038a8a11
Ron <varakt.23@gmail.com>
2024-02-10 20:11:22 +0000

    Feb 10, 2024, 3:11 PM

--- CCC-Senior/2006/Tin.py
+++ CCC-Senior/2006/Tin.py
@@ -15,5 +15,11 @@ for _ in range(N):
     # All on one line, ofc
 
     temp = list(map(int, input().split()))
-
+    
     corner_count = temp[0]
+    for j in range(0, corner_count - 2, 2):
+        if ((temp[j] >= Romy[0] and temp[j] <= Jules[0]) or (temp[j] >= Jules[0] and temp[j] <= Romy[0])) and (temp[j+2] >= Romy[0] and temp[j+2] <= Jules[0]) or (temp[j+2] >= Jules[0] and temp[j+2] <= Romy[0]):
+            # If the x-coordinates are between/on the house(s)
+            if temp[j+1] >= Romy[1] and temp[j+1] <= Jules[1] or temp[j+1] >= Jules[1] and temp[j] <= Romy[1]:
+                # AND the y-coordinates are between/on the house(s)
+                violations += 1
