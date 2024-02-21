627766290dc5e83103bee1f2c9035361152bd0cd
Ron <varakt.23@gmail.com>
2024-02-21 02:00:31 +0000

    Feb 20, 2024, 9:00 PM

--- CCC-Senior/2019/Triangle.py
+++ CCC-Senior/2019/Triangle.py
@@ -1,4 +1,11 @@
 # Triangle: The Data Structure; IN PROGRESS
 # https://cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2019/stage%201/seniorEF.pdf # 5
 
+N, K = list(map(int, input().split()))
+# N is the number of rows
+# K is the size of triangle to check
+triangle = []
+max_sum = 0
+for _ in range(N):
+    triangle.append(list(map(int, input().split())))
 
