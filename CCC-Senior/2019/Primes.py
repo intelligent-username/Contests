ccb39a3354d0b711210ed1816dca27184b61a1a2
Ron <varakt.23@gmail.com>
2024-02-21 01:42:24 +0000

    Feb 20, 2024, 8:42 PM

--- CCC-Senior/2019/Primes.py
+++ CCC-Senior/2019/Primes.py
@@ -6,40 +6,23 @@
 # A combination of two primes
 # Such that the average of these primes
 # Is that number
-def main():
-    T = int(input()) # Number of test cases
-    requirements = []
-
-    # Integers between 4 and 1 billion
-    for _ in range(T):
-        requirements.append(int(input()))
-
-
-
-def prime_finder(num):
-    # If the number is even (remember it's greater than 2)
-    # Then not prime obviously
-    if not num % 2:
+def is_prime(num):
+    if num < 2:
         return False
-    
-    digit_sum = sum(int(digit) for digit in str(abs(num)))
-
-    # If the sum of the digits is divisible by 3, return (not necessary? delete?)
-    if digit_sum % 3:
-        return False
-    
-    temp = 3
-    while temp < num:
-    # for j in range(3, num, 2), not checking even numbers obviously:
-        if num // temp == 0: # if divisble OK
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
-        temp += 2
-    
-    if temp == num:
-        return [temp, temp]
-    
-    
-    
+    return True
 
-if __name__ == "__main__":
-    main()
+def find_primes_for_average(N):
+    # Iterate from 2 to N/2 to find two primes whose average is N
+    for A in range(2, N // 2 + 1):
+        B = N - A
+        if is_prime(A) and is_prime(B):
+            return [A, B]
+
+T = int(input())  # Number of test cases
+for _ in range(T):
+    N = int(input())  # Input N for each test case
+    primes = find_primes_for_average(N)
+    print(primes[0], primes[1])
