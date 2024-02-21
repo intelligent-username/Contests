9fec17c9d4e47bbf5f2620dc4e85bf4fc38077f8
Ron <varakt.23@gmail.com>
2024-02-21 01:26:03 +0000

    Feb 20, 2024, 8:26 PM

---
+++ CCC-Senior/2019/Fliper.py
@@ -0,0 +1,21 @@
+# Flipper: COMPLETE
+# https://www.cemc.uwaterloo.ca/contests/computing/past_ccc_contests/2019/stage%201/juniorEF.pdf #4
+
+# H = Horizontal Flip
+# V = Vertical Flip
+original = [1, 2, 3, 4]
+
+flips = input()
+H = flips.count('H') % 2
+V = flips.count('V') % 2
+
+
+if H:
+    original[0], original[1] = original[1], original[0]
+    original[2], original[3] = original[3], original[2]
+if V:
+    original[0], original[2] = original[2], original[0]
+    original[1], original[3] = original[3], original[1]
+
+print(f"{original[0]} {original[1]}\n{original[2]} {original[3]}")
+
