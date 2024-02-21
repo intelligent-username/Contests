6ad6507a2718eeadcc8790ae4a6cf1150d65a581
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2024-02-21 15:33:59 +0000

    Feb 21, 2024, 3:33 PM

--- CCC-Senior/2024/Heavy.py
+++ CCC-Senior/2024/Heavy.py
@@ -1,16 +1,18 @@
 # Heavy-Light Composition
 
-def initial_check(list):
-    char1 = list[0]
-    char2 = list[1]
-    length = len(list)
-    for j in range(0, length, 2):
-        try:
-            if list[j] != char1 and list[j+1] != char2:
-                return False
-        except:
-            pass
+# Check if alternating letters exist
+def initial_check(list, char1, char2):
     
+    length = len(list)
+    
+    for x in range(0, length, 2):
+        if list[x] != char1:
+            return False
+    
+    for r in range(1, length, 2):
+        if list[r] != char2:
+            return False
+
     return True
 
 # A letter is heavy if it appears more than once
@@ -28,8 +30,15 @@ for _ in range(T):
 
 # Output
 for string in inputs:
-    if initial_check(string):
+    char1 = list[0]
+    char2 = list[1]
+    if initial_check(string, char1, char2):
         print("The letters are alternating")
+        if (string.count(char1) > 1 and string.count(char2) < 2) or (string.count(char2) > 1 and string.count(char1) < 2):
+            print("T")
+        else:
+            print("F")
+
     else:
         print("The letters aren't alternating")
         print("F")
