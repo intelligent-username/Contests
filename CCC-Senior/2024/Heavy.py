6a71dde6e647f00f39eb97c1b895f9e3f439d68a
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2024-02-21 14:58:01 +0000

    Feb 21, 2024, 2:58 PM

--- CCC-Senior/2024/Heavy.py
+++ CCC-Senior/2024/Heavy.py
@@ -19,14 +19,17 @@ for string in inputs:
     light = set()
 
     for i in range(1, N):
-        if string[i] != string[i - 1]:
+        if string[i] != string[i+2] and string[i+1] != string[i+1]:
+            otuputs.append(False)
+            break
+        if string[i] == string[i - 1]:
             outputs.append(False)
             break
         elif string[i] not in light:
             light.add(string[i])
         
         elif string[i] in heavy:
-            
+
         
         else: # Final condition; it's in light, so must be added to heavy
             heavy.add(string[i])
