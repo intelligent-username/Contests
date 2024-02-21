83ee1886046443a42c00b3d95bf6b03bd898924c
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2024-02-21 14:34:12 +0000

    Feb 21, 2024, 2:34 PM

--- CCC-Senior/2024/Heavy.py
+++ CCC-Senior/2024/Heavy.py
@@ -3,15 +3,30 @@
 # A letter is heavy if it appears more than once
 # Otherwise it's light
 
-T, N = list(map(int,input().split()))
-
+# Input
+T, N = list(map(int, input().split()))
 inputs = []
-heavy = []
 
-for x in range(T):
-    inputs.append(input().split())
+# Collect input strings
+for _ in range(T):
+    inputs.append(input().strip())
 
-# Determine if they're alternating heavy letters
-for r in inputs:
-    for j in r:
+# Output
+for string in inputs:
+    alternating = True
+    heavy = set()
+    light = set()
 
+    for i in range(1, N):
+        if string[i] == string[i - 1]:
+            alternating = False
+            break
+        elif string[i] not in light:
+            light.add(string[i])
+        elif string[i] not in heavy:
+            heavy.add(string[i])
+        else:
+            alternating = False
+            break
+
+    print("T" if alternating else "F")
