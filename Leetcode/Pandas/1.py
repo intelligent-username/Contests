f82650407adb73c636751ea3738c4eadd0802c6f
Ron <varakt.23@gmail.com>
2024-03-16 14:43:13 +0000

    Mar 16, 2024, 10:43 AM

--- Leetcode/Pandas/1.py
+++ Leetcode/Pandas/1.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
         return pd.DataFrame(student_data, columns=['student_id', 'age'])
+
+print("Test")
