68da8a1f73725ad10e7b1689128640fb10477d0d
Ron <varakt.23@gmail.com>
2024-04-13 20:56:07 -0400

    2

---
+++ Leetcode/SQL50/6.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT CASE WHEN EUNI.unique_id IS NULL THEN NULL ELSE EUNI.unique_id END AS unique_id, E.name
+FROM Employees AS E
+LEFT JOIN EmployeeUNI AS EUNI ON E.id = EUNI.id;
