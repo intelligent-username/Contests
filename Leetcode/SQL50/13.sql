0ec388cb32bea3f403149cd7f7f74132131b809d
Ron <varakt.23@gmail.com>
2024-04-15 19:48:09 -0400

    aiodn

---
+++ Leetcode/SQL50/13.sql
@@ -0,0 +1,8 @@
+--         https://leetcode.com/problems/managers-with-at-least-5-direct-reports/?envType=study-plan-v2&envId=top-sql-50
+--         Managers with at least 5 ...
+
+SELECT m.name
+FROM Employee e
+JOIN Employee m ON e.managerId = m.id
+GROUP BY m.id, m.name
+HAVING COUNT(e.id) >= 5
