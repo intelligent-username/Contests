a636d521f51228bc1b2bdb8f4f4ec09316b9aeeb
Ron <varakt.23@gmail.com>
2024-04-17 18:33:51 -0400

    ASNODNDIFBS

--- Leetcode/SQL50/16.sql
+++ Leetcode/SQL50/16.sql
@@ -1,10 +1,10 @@
 --      https://leetcode.com/problems/average-selling-price/description/?envType=study-plan-v2&envId=top-sql-50
 --      Average Selling Price
---      INCOMPLETE; 15/17 pass
+--      COMPLETE
 
-SELECT 
-    p.product_id, 
-    ROUND(SUM(u.units * p.price) / SUM(u.units), 2) AS average_price
+SELECT p.product_id, 
+       ROUND(COALESCE(SUM(u.units * p.price) / SUM(u.units), 0), 2) AS average_price
 FROM Prices p
-JOIN UnitsSold u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+LEFT JOIN UnitsSold u ON p.product_id = u.product_id
+                     AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
