69d860745a0811c5f44bf3425d52337eba075bc2
Ron <varakt.23@gmail.com>
2024-04-17 20:27:22 -0400

    oiadn

---
+++ Leetcode/18.sql
@@ -0,0 +1,5 @@
+    -- https://leetcode.com/problems/percentage-of-users-attended-a-contest/submissions/1235308094/?envType=study-plan-v2&envId=top-sql-50
+    -- Percentage of Users
+    -- Incomplete
+
+https://leetcode.com/problems/percentage-of-users-attended-a-contest/submissions/1235308094/?envType=study-plan-v2&envId=top-sql-50
