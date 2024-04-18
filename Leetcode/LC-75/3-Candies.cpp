3ff626d360716b755f94a9bc94563d972c5154f2
Ron <varakt.23@gmail.com>
2024-04-18 19:43:13 -0400

    aiodn

---
+++ Leetcode/LC-75/3-Candies.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> res(candies.size(), false);
+        int max_candies = *(max_element(candies.begin(), candies.end()));
+        for(int i=0;i<candies.size();i++){
+            res[i] = (max_candies <= candies[i] + extraCandies);
+        }
+        return res;
+    }
+};
