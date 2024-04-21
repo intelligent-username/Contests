ac7d3856b22ff1d7c32ab6dbddd3e5e3253de216
Ron <varakt.23@gmail.com>
2024-04-21 22:39:08 +0000

    Apr 21, 2024, 6:39 PM

---
+++ Leetcode/30_Days_Pandas/2.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+# IN PROGRESS
+
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products['low_fats'] == "Y") & (products['recyclable'] == "Y")]['product_id']    
