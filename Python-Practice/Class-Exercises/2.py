88a6063bcd871b2e10ad9329ce5684a13df64094
Ron <varakt.23@gmail.com>
2024-04-21 21:52:56 +0000

    Apr 21, 2024, 5:52 PM

---
+++ Python-Practice/Class-Exercises/2.py
@@ -0,0 +1,27 @@
+from random import randint
+
+class example:
+    x = randint(1, 100)
+    def __init__(self, a, b = 12, c = 33):
+        self.a= a
+        self.b = b
+        self.c = c
+
+    if x % 2:
+        # X is odd
+        d = b * c + a
+    else:
+        d = b * a + c
+
+
+    def display(self):
+        print("d =", b * a + c)
+    
+        
+example(1,2,3)
+# Not useful but might as well use
+
+for name in example.__dict__:
+    print(name)
+
+
