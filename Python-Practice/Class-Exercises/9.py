5515892898e66a974480c97b230f63f169152e9b
Ron <varakt.23@gmail.com>
2024-05-04 10:32:26 -0400

    ngioanioganaiofbeuibf

---
+++ Python-Practice/Class-Exercises/9.py
@@ -0,0 +1,26 @@
+class Student:
+    school = "ABC School"
+
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = [grade]
+    
+    def grade_changer(self, grade):
+        self.grade.append(grade)
+    
+    def name_changer(self, name):
+        self.name = name
+
+Jimmy = Student("Jimmy", "95")
+
+print("Jimmy's profile:", Jimmy)
+print("Jimmy's Grades:", Jimmy.grade)
+
+Jimmy.grade_changer("22")
+
+Jimmy.name_changer("Jimmy Peter")  
+print("Jimmy's profile updated:", Jimmy)
+print("Jimmy's Grades updated:", Jimmy.grade)
+
+setattr(Student, "school", "ABCDEF School")
+print("School name:", Jimmy.school)
