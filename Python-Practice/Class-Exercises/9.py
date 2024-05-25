4d87bfea0ddc3561609c56cd9b004ae17e4a98de
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2024-05-24 21:54:18 -0400

    Update 9.py

--- Python-Practice/Class-Exercises/9.py
+++ Python-Practice/Class-Exercises/9.py
@@ -1,5 +1,5 @@
 class Student:
-    school = "ABC School"
+    school = "ABC Academy"
 
     def __init__(self, name, grade):
         self.name = name
@@ -22,5 +22,5 @@ Jimmy.name_changer("Jimmy Peter")
 print("Jimmy's profile updated:", Jimmy)
 print("Jimmy's Grades updated:", Jimmy.grade)
 
-setattr(Student, "school", "ABCDEF School")
+setattr(Student, "school", "ABCDEF Collegiate")
 print("School name:", Jimmy.school)
