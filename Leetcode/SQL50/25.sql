0623bac0da52c0a621d63a869055a9005c9bf6d5
Ron <varakt.23@gmail.com>
2024-05-30 21:37:19 -0400

    ifpen

---
+++ Leetcode/SQL50/25.sql
@@ -0,0 +1,17 @@
+SELECT 
+    s.product_id, 
+    MIN(s.year) AS first_year, 
+    s.quantity, 
+    s.price
+FROM 
+    Sales s
+JOIN 
+    (SELECT product_id, MIN(year) AS first_year
+     FROM Sales
+     GROUP BY product_id) f
+ON 
+    s.product_id = f.product_id 
+AND 
+    s.year = f.first_year
+GROUP BY 
+    s.product_id, s.year, s.quantity, s.price;
