32fa8d90fe9bc23fa07d2d5093283993dd8a83ae
Ron <varakt.23@gmail.com>
2024-06-12 10:34:42 -0400

    hh

---
+++ Leetcode/SQL50/26.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/classes-more-than-5-students/description/?envType=study-plan-v2&envId=top-sql-50
+--  Classes with more than 5 people
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
