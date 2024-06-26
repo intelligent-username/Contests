7ae756d461fa3b90091a5604d7259ccc956d955c
Ron <varakt.23@gmail.com>
2024-06-26 13:47:01 -0400

    2 SQL

--- Leetcode/SQL50/29.sql
+++ Leetcode/SQL50/29.sql
@@ -1,4 +1,7 @@
 -- https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50
 -- Customers Who Bought All Products
 
-
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
