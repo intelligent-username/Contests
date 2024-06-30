d2d208886a75c8231fac3abf7b79bc66044f7cc0
Ron <varakt.23@gmail.com>
2024-06-29 21:47:05 -0400

    dien

--- Leetcode/SQL50/31.sql
+++ Leetcode/SQL50/31.sql
@@ -1,8 +1,13 @@
 -- https://leetcode.com/problems/primary-department-for-each-employee/description/?envType=study-plan-v2&envId=top-sql-50
 -- Primary Department for Each Employee
 
-SELECT employee_id, 
-       COALESCE(MAX(CASE WHEN primary_flag = 'Y' THEN department_id END), MIN(department_id)) AS department_id
+SELECT employee_id, department_id
 FROM Employee
-GROUP BY employee_id;
+WHERE primary_flag = 'Y'
 
+UNION
+
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1;
