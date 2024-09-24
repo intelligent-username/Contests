a9326a79388cedbbf4251133f58cfa209b5af8b9
intelligent-username <varakt.23@gmail.com>
2024-09-23 21:53:16 -0400

    a

---
+++ Leetcode/LC-75/python-version/7.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=leetcode-75
+# Product of Array Except Self
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        answer = [1] * n
+        left_prod = 1
+        for i in range(n):
+            answer[i] = left_prod
+            left_prod *= nums[i]
+        
+        right_prod = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= right_prod
+            right_prod *= nums[i]
+        
+        return answer
