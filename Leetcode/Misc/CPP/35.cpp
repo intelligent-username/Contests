47d0298617d46f4decb2e865e32d600cbd095935
Varak <varakt.23@gmail.com>
2025-12-18 23:59:14 -0500

    New daily

---
+++ Leetcode/Misc/CPP/35.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        
+        set<int> knowers;
+
+        knowers.insert(0);
+        knowers.insert(firstPerson);
+
+        // This is the position for time
+        int t = 2;
+        
+        // Sort by time for convenience
+        // Maybe sorting isn't necessary but I can't think of any other way
+        sort(meetings.begin(), meetings.end(), 
+        [t](const vector<int>& a, vector<int>& b) {
+            return a[t] <= b[t];
+        })
+
+        int i = 0;
+        int len = meetings.size();
+
+        while (i < len) {
+            int time = meetings[i][t];
+
+            vector<pair<int,int>> curr;
+
+            while (i < len && meetings[i][t] == time) {
+                curr.push_back({meetings[i][0], meetings[i][1]});
+                i++;
+            }
+
+            // Now we've formed our current batch. The biggest problem is 
+            // sharing the secret to multiple people simultaneously
+            // Then search to find connections
+            // And add to knowers
+
+            unordered_map<int, vector<int>> graph;
+            for (auto &meeting : curr) {
+                int x = meeting.first;
+                int y = meeting.second;
+                graph[x].push_back(y);
+                graph[y].push_back(x);
+            }
+
+            queue<int> q;
+            unordered_set<int> visited;
+
+            for (auto &[u, _] : graph) {
+                if (knowers.count(u)) {
+                    q.push(u);
+                    visited.insert(u);
+                }
+            }
+
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                knowers.insert(u);
+
+                for (int v : graph[u]) {
+                    if (!visited.count(v)) {
+                        visited.insert(v);
+                        q.push(v);
+                    }
+                }
+            }
+
+        }
+
+        // Convert knowers to list :)
+
+        vector<int> answer(knowers.begin(), knowers.end());
+
+        // And we're done
+
+        return answer;
+
+    }
+};
